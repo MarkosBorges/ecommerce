3e98ce5192d39f6e8c1a30786c16a08a40ba9d72
Markos Borges <markous_k@protonmail.com>
2021-04-13 16:54:29 -0300

    pageadmin template

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once("vendor/autoload.php"); // traz as dependências
 //carrrega classes 
 use \Slim\Slim; 
 use \helping\Page;
-
+use \helping\PageAdmin;
 
 $app = new Slim(); //rota | manda para algum lugar
 
@@ -26,6 +26,20 @@ $app->get('/', function() { //chamar sem nenhum tipo de rota
 
 });
 
+$app->get('/admin', function() { //chamar sem nenhum tipo de rota
+    
+	//$sql = new helping\DB\Sql();
+
+	//$results = $sql->select("SELECT * FROM tb_users");
+
+	//echo json_encode($results);
+	//var_dump($results);
+	$page = new PageAdmin(); //cria nova página 
+	$page->setTpl("index"); //carrega conteúdo html principal
+
+
+
+});
 $app->run(); 
 
  ?>
---
+++ vendor/helping/php-classes/src/pageAdmin.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace helping;
+
+/**
+ * 
+ */
+class PageAdmin extends Page{
+	
+	public function __construct($opt = array(), $tpl_dir = "/views/admin/"){
+
+		parent::__construct($opt, $tpl_dir);
+
+	}
+
+}
+
+
+
+?>
